6d4ffb539d7b8b565c03988adcdd5e8feafb95d5
Esteban RdzLl <estebanrodriguezll@gmail.com>
2017-10-19 20:37:20 -0500

    ProjectAssigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script contains 2 functions, one that generates a list of functions for
+## the second function to use, which actually calcullates the inverse of the 
+## data input
 
-## Write a short comment describing this function
+## This function initializes both the original matrix and the inverse as global 
+## (parent) values, then creates a set of functions which work as setters and 
+## getters for future referencing and finally makes a named list of this 
+## functions
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+        # dM<-dim(M)
+        inv <- NULL
+        set <- function(N) {
+                M <<- N
+                inv <<- NULL
+        }
+        get <- function() M
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
+## This functions takes an object of type "makeCacheMatrix" and uses this object 
+## to access the functions in it (see previous function) to check for a possible
+## value of the inverse matrix. If it exists, it takes the value located in the 
+## parent directory and if it doesn't it computes it and save it to the memory.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mkMat, ...) {
+        inv <- mkMat$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        M <- mkMat$get()
+        inv <- solve(M)
+        mkMat$setinv(inv)
+        inv
 }
